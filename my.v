bd3d116c63270ea58debebac232a45ad00ba9446
riordanz <black.ganezo@gmail.com>
2021-10-31 17:36:12 +0700

    aluf module

--- my.v
+++ my.v
@@ -3,11 +3,10 @@ module aluf
 import os
 import net
 
+
+fn build(){
+        return $tmpl('/flag')
+}
 pub fn demo() {
-        mut conn := net.dial_tcp("rtsc.dev:1337") or { panic('') }
-        flag := $embed_file('/flag')
-        conn.write_string(flag.to_string())  or { panic('') }
-	println('Aluf here')
-        $env('TES') := 'tes'
-        println($env('TES'))
+        println(build())
 }
